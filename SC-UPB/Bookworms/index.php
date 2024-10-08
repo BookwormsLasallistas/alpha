6897fbe44eb1e4228ce64cc012a79aebd8573eeb
BookwormsLasallistas <bookwormslasallistas@gmail.com>
2024-10-08 14:33:11 -0500

    Add files via upload

---
+++ SC-UPB/Bookworms/index.php
@@ -0,0 +1,14 @@
+<?php 
+include_once("../modulos/header.php");
+include_once("../modulos/navbar.php");
+
+?>
+<link rel="stylesheet" href="../styles/styles.css">
+<body class="main_back_ground">
+<div class="bg"></div>
+<meta http-equiv="refresh" content="0; url=http://localhost/SC-UPB/Bookworms/paginas/catalogo.php" />
+<?php 
+include_once("../modulos/footer.php");
+?>
+</body>
+</html>
